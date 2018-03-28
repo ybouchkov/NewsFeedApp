6183cafb6c960840f98006635702169894bda946
Yani Bouchkov <Yani.Buchkov@adastragrp.com>
2018-03-28 18:09:41 +0300

    🎨  Implement UITableView extensions

--- NewsFeedApp/NewsFeedApp/Extensions/UITableView+.swift
+++ NewsFeedApp/NewsFeedApp/Extensions/UITableView+.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2018 Mr Bouchkov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func registerCell<T:UITableViewCell>(_ cellType: T.Type) {
+        let name = String(describing: cellType)
+        let nib = UINib(nibName: name, bundle: nil)
+        self.register(nib, forCellReuseIdentifier: name)
+    }
+    func registerCells<T:UITableViewCell>(_ cellTypes: [T.Type]) {
+        for cellType in cellTypes {
+            let name = String(describing: cellType)
+            let nib = UINib(nibName: name, bundle: nil)
+            self.register(nib, forCellReuseIdentifier: name)
+        }
+    }
+    func registerHeader<T:UITableViewHeaderFooterView>(_ headerType: T.Type) {
+        let name = String(describing: headerType)
+        let nib = UINib(nibName: name, bundle: nil)
+        self.register(nib, forHeaderFooterViewReuseIdentifier: name)
+    }
+    func dequeueCell<T:UITableViewCell>(_ cellType: T.Type) -> T {
+        let name = String(describing: cellType)
+        guard let cell = dequeueReusableCell(withIdentifier: name) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(name)")
+        }
+        return cell
+    }
+    func dequeueHeader<T:UITableViewHeaderFooterView>(_ headerType: T.Type) -> T {
+        let name = String(describing: headerType)
+        guard let cell = dequeueReusableHeaderFooterView(withIdentifier: name) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(name)")
+        }
+        return cell
+    }
+}
